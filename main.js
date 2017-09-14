8d71cddd6513852f00bd3f125de97240b51b698e
Sumer Mixon <sumer@ximm.us>
2017-09-14 09:17:31 -0400

    setting up xml request

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("It's ALIVE!!");
+function loadDoc() {
+	
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() {
+		
+		if (this.readyState == 4 && this.status == 200) {
+
+			document.getElementById("demo").innerHTML = this.responseText;
+
+		}
+	};
+
+	xhttp.open("GET", "ajax_info.txt", true);
+	xhttp.send();
+
+}
